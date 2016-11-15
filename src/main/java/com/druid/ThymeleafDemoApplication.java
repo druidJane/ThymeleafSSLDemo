fa4409d4023fa2f4279b6ff720126f2e76e16040
self <413429011@qq.com>
2016-11-15 09:49:42 +0800

    加入SSL例子

--- src/main/java/com/druid/ThymeleafDemoApplication.java
+++ src/main/java/com/druid/ThymeleafDemoApplication.java
@@ -1,11 +1,19 @@
 package com.druid;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Security;
 import java.util.ArrayList;
 
 @Controller
@@ -28,6 +36,32 @@ public class ThymeleafDemoApplication {
 
 		return "index";
 	}
+	@Bean
+	public EmbeddedServletContainerFactory servletContainerFactory(){
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+	@Bean
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafDemoApplication.class, args);
 	}
